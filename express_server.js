659dc80728cfa8befc92112950b8afa3800c0fa9
Mel Smith <Mel@Mels-Air-2.hitronhub.home>
2017-11-21 23:41:27 -0800

    completed url shortening part 1, randomgstring function added & urls_new template created

--- express_server.js
+++ express_server.js
@@ -1,15 +1,27 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; 
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 8080; 
 
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
   "hf876d": "http://wikipedia.com"
 };
+
+function generateRandomString() {
+  let randomString = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < 6; i++){
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomString;
+}
 
 app.get("/", (req, res) => {
   res.end("Hello!");
@@ -20,9 +32,20 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', data);
 });
 
+
+
 app.get("/urls/:id", (req, res) => {
   let data = { shortURL: req.params.id, longURL: urlDatabase[req.params.id], };
   res.render("urls_show", data);
+});
+
+app.get('/urls/new', (req, res) => {
+
+  if (!req.session.user_id) {
+    res.redirect('/login');
+  } else {
+    res.render('urls_new');
+  }
 });
 
 
